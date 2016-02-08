c7f6d84917b3161ea117c3bdaf3be492f1be5f74
Chris Marzullo <sevenmanin@gmail.com>
2016-02-08 13:41:51 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -13,14 +13,25 @@ config = {
     'version': '2.0',
     'install_requires': ['nose','mako'],
     'packages': ['saltscaffold'],
+    'package_data': {
+        '': [
+            'skel/*.md',
+            'skel/*.txt',
+            'skel/*.sls',
+            'skel/formula/*.sls',
+            'skel/formula/map.jinja',
+            'skel/formula/defaults.yml',
+            'skel/formula/files/config.conf',
+            'skel/test/integration/default/serverspec/_spec.rb'
+            ]
+        },
     'scripts': [],
     'name': 'Saltscaffold',
     'entry_points': {
         'console_scripts': [
             'saltscaffold = saltscaffold.__main__:main',
-        ]
-    }
-
+            ]
+        }
 }
 
 setup(**config)
