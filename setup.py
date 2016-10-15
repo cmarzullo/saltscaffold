b8c763fe3a6d733941169f581a2281df03605590
Chris Marzullo <sevenmanin@gmail.com>
2016-10-15 09:47:40 -0400

    include long desc

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
 config = {
     'description': 'sets up files and directories for a new salt formula',
+    'long_description': read_md('README.md'),
     'author': 'Christopher Marzullo',
     'url': 'https://github.com/cmarzullo/saltsaffold',
     'author_email': 'cmarzullo@linode.com',
-    'version': '3.0.1',
+    'version': '3.0.2',
     'install_requires': ['nose','mako'],
     'packages': ['saltscaffold'],
     'package_data': {
